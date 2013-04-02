c47216feff6450c7f9a9050e69bd38c358fa6a59
Rastii <rastii09@gmail.com>
2013-04-01 22:31:20 -0400

    Added a bunch of stuff -- Login/Registration AND api access

--- application.py
+++ application.py
@@ -1,14 +1,81 @@
 import flask
- 
-application = flask.Flask(__name__)
+from flask import Flask, request, session, redirect, flash, json, \
+	render_template, abort, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from hashlib import sha256 
+import bcrypt
 
-#Set application.debug=true to enable tracebacks on Beanstalk log output. 
-#Make sure to remove this line before deploying to production.
-application.debug=True
- 
+application = flask.Flask(__name__)
+application.config.from_pyfile('application.cfg')
+db = SQLAlchemy(application)
+
+
 @application.route('/')
 def hello_world():
     return "Hello world!"
+
+
+@application.route('/register', methods=['POST'])
+def register_user():
+	if all (x in request.form for x in ('username', 'password')):
+		salt = bcrypt.gensalt()
+		password = bcrypt.hashpw(request.form['password'], salt)
+		query = '''
+			INSERT INTO users VALUES(NULL, :username, :salt, :hash);
+		'''
+		try:
+			result = db.session.execute(query, {
+				'username': request.form['username'],
+				'salt': salt,
+				'hash': password
+			})
+			db.session.commit()
+			return '1'
+		except:
+			return '-1' #error in the query... (user exists)
+	else:
+		return '0' #missing post variables!
+
+@application.route('/login', methods=['POST'])
+def login_user():
+	if all (x in request.form for x in ('username', 'password')):
+		query = '''
+			SELECT salt, password FROM users WHERE username=:username
+		'''
+		try:
+			tupl = db.session.execute(query, {
+				'username':request.form['username']
+			}).first()
+			salt, password = tupl
+			print 'retrieved salt: ' + salt
+			print 'retrieved hash: ' + password
+			if(bcrypt.hashpw(request.form['password'], salt) == password):
+				return '1'
+			else:
+				return '0'
+		except:
+			return '0'
+	else:
+		return '-1'
+	
+@application.route('/api/<search_term>', methods=['GET'])
+def get_ingredients(search_term):
+	data = []
+	query = '''
+		SELECT name, calories FROM ingredients WHERE name LIKE :term
+	'''
+	try:
+		projects = db.session.execute(query, {
+			'term': '%' + search_term + '%'
+		})
+		for name, calories in projects:
+			data.append({
+				'name': name,
+				'calories': calories
+			})
+		return json.dumps(data)
+	except:
+		return '-1'
  
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
